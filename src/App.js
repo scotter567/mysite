69d64e28b466302ac90a5e1c0116b4ea0f1d4334
Jordan <scotter567@gmail.com>
2024-10-13 23:59:39 -0400

    added things for deploy

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
 import './styles.css';
-
+import MePNG from './icons/avi-nobg.png'
+import twitchIcon from './icons/twitch.svg'
+import twitterIcon from './icons/twitter-x.svg'
+import youtubeIcon from './icons/youtube.svg'
 function App() {
   return (
-    <h1>HELLO WORLD!</h1>
+    <div className='mainContainer'>
+      <div className='aviContainer'>
+        <img id='avi' src={MePNG}></img>
+      </div>
+      <div className='iconContainer'>
+        <a href='https://twitch.tv/statelyfall'>
+          <img className='icon' src={twitchIcon}></img>
+        </a>
+        <a href='https://x.com/StatelyFall'>
+          <img className='icon' src={twitterIcon}></img>
+        </a>
+        <a href='https://www.youtube.com/@jordanscott5525'>
+          <img className='icon' src={youtubeIcon}></img>
+        </a>
+      </div>
+      <div className='linkContainer'>
+        <div className='link'>
+          Link 1
+        </div>
+        <div className='link'>
+          Link 1
+        </div>
+        <div className='link'>
+          Link 1
+        </div>
+      </div>
+    </div>
   );
 }
 
